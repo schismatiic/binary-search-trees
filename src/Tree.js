e5794755a08c2a737d6e1a575f6101972b0aefaa
schismatiic <leonardojofre2003@gmail.com>
2026-05-18 14:50:46 -0400

    Sort and remove duplicates

--- src/Tree.js
+++ src/Tree.js
@@ -2,10 +2,27 @@ import { Node } from "./Node.js";
 
 class Tree {
   constructor(arr = [], root = null) {
-    this.arr = arr;
-    this.root = root;
+    this.arr = this.sortArray(arr);
+    this.root = this.buildTree(this.arr);
   }
-  buildTree(arr, start, end) {
+  sortArray = (arr) => {
+    const sortedArray = arr.sort((a, b) => {
+      return a - b;
+    });
+    const removeDuplicate = sortedArray.filter(
+      (element, index, arr) => element !== arr[index - 1],
+    );
+    return removeDuplicate;
+  };
+  prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null || node === undefined) {
+      return;
+    }
+    this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  };
+  buildTree(arr, start = 0, end = arr.length - 1) {
     if (start > end) return null;
 
     let mid = start + Math.floor((end - start) / 2);
@@ -14,9 +31,6 @@ class Tree {
     root.left = this.buildTree(arr, start, mid - 1);
     root.right = this.buildTree(arr, mid + 1, end);
     return root;
-  }
-  sortedArrayToBST(arr) {
-    return this.buildTree(arr, 0, arr.length - 1);
   }
 }
 export { Tree };
